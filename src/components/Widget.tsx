aa6e7e0f465efc74fa594821e0152fb2f5de0161
matheusoliv23 <matheushenri1996@gmail.com>
2022-05-03 12:30:15 -0300

    feat Widget Component

---
+++ src/components/Widget.tsx
@@ -0,0 +1,29 @@
+import { ChatTeardropDots } from "phosphor-react";
+import React, { useState } from "react";
+import { Popover } from "@headlessui/react";
+
+export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
+  return (
+    <Popover className="absolute bottom-5 right-4">
+      <Popover.Panel>Testando</Popover.Panel>
+
+      <Popover.Button
+        onClick={toggleWidgetVisibility}
+        className="bg-violet-500 rounded-full px-3 h-12 text-white  flex items-center group"
+      >
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+}
