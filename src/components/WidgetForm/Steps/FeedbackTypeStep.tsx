f0d85154dffbc7d3925d8b0647b3ec65bb514186
matheusoliv23 <matheushenri1996@gmail.com>
2022-05-03 21:37:42 -0300

    Logic for changing feedback types when clicling

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from "..";
 
 interface FeedbackStepProps {
@@ -7,19 +8,26 @@ interface FeedbackStepProps {
 
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackStepProps) {
   return (
-    <div className="flex py-8 gap-2 w-full">
-      {Object.entries(feedbackTypes).map(([key, value]) => {
-        return (
-          <button
-            key={key}
-            className="bg-zinc-800 rouded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500"
-            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-          >
-            <img src={value.image.source} alt={value.image.alt} />
-            <span>{value.title}</span>
-          </button>
-        );
-      })}
-    </div>
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rouded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { CloseButton } from "../CloseButton";
-
 //colocar redes sociais no footer
 
 import bugImgUrl from "../../assets/Bug.svg";
 import ideiaImgUrl from "../../assets/Idea.svg";
 import otherImgUrl from "../../assets/Thought.svg";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import FeedbackContent from "./Steps/FeedbackContent";
 
 export const feedbackTypes = {
   BUG: {
@@ -37,17 +36,19 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
-  return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto transition duration-1000 ease-in-out">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
 
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-3xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto transition duration-1000 ease-in-out">
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <div>hello </div>
+        <FeedbackContent
+          feedbackType={feedbackType}
+          onFeedbackRestartRequest={handleRestartFeedback}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
