54ed0c4b6d426169c97600516272900ab2e56ced
matheusoliv23 <matheushenri1996@gmail.com>
2022-05-04 14:07:33 -0300

    feat Buttons

--- src/components/WidgetForm/Steps/FeedbackContent.tsx
+++ src/components/WidgetForm/Steps/FeedbackContent.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import React from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
@@ -41,6 +41,21 @@ export default function FeedbackContent({
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes"
         />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="bg-zinc-800 p-2 rouded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 "
+          >
+            <Camera className="w-6 h-6" />{" "}
+          </button>
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
       </form>
     </>
   );
